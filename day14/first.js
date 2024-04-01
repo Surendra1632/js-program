46b2039c82f74e3e7621498bd6d85eb49ea77646
Surendra1632 <surendra.lateral2k22@mpgi.edu.in>
2024-04-01 23:53:17 +0530

    dom menupulation

---
+++ day14/first.js
@@ -0,0 +1,5 @@
+let div = document.querySelectorAll(".box");
+console.log(div.innerText);
+div[0].innerText = "new unique value 1";
+div[1].innerText ="new unique value 2";
+div[2].innerText ="new unique value 3";
